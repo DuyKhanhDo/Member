165ccbd8ef248a13844c824712437580cc22cb9c
DK <duykhanh02012004@gmail.com>
2024-01-10 01:38:37 +0000

    Basically finished

--- slider.php
+++ slider.php
@@ -1,4 +1,4 @@
-<section class="member-content">
+
         <div class="member-content-left">
             <ul>
                 <li><a href="#">Workout Routine</a>
@@ -9,30 +9,13 @@
                 </li>
                 <li><a href="#">Nutritrion Information</a>
                     <ul>
-                        <li><a href="#">Nutritrion List</a></li>
-                        <li><a href="#">Nutrition Day</a></li>
+                        <li><a href="nutritionList.php">Nutritrion List</a></li>
+                        <li><a href="nutritionAdd.php">Nutrition Day</a></li>
                     </ul>
                 </li>
-                <li><a href="#">Diet Plan</a>
-                    <ul>
-                        <li><a href="#"></a>View Plan</li>
-                        <li><a href="#"></a>Edit Plan</li>
-                    </ul>
-                </li>
-                <li><a href="#">Reveal PIN</a>
             </ul>
 
 
         </div>
 
-        <div class="member-content-right">
-            <div class="member-content-right-list">
-                <h1>Workout Day</h1>
-                <form action="" method="POST"></form>
-                    <input type="text" placeholder="Add excercise">
-                    <button type="submit">Add</button>
-            </div>
-
-        </div>
-
-    </section>
+      
--- workoutAdd.php
+++ workoutAdd.php
@@ -1,56 +1,48 @@
 <?php
 include "config.php";
 include "header.php";
-include "slider.php";
+include "Class/WorkoutClass.php";
+include "database.php";
+$workout = new Workout;
 
-session_start(); // Start the session at the beginning of your script
-
-// Assuming you have a form submission to add exercises
 if (isset($_POST['submit']) && !empty($_POST['exercise'])) {
-    // Initialize the session variable as an array if not set
-    if (!isset($_SESSION['exercises'])) {
-        $_SESSION['exercises'] = array();
-    }
-    // Add the new exercise to the session array
-    $_SESSION['exercises'][] = $_POST['exercise'];
+    $exerciseName = $_POST['exercise'];
+    $status = $workout->insert_workout($exerciseName);
 }
+$exercises = $workout->get_today_exercises();
+$total_caloriesBurned = 0;
+$total_duration = 0;
+  if (isset($excercies->num_rows)) {
+                    while($exercise = mysqli_fetch_assoc($exercises)){
+                       $total_duration += $exercise['duration'];
+                        $total_caloriesBurned += $exercise['caloriesBurned'];
+                    }
+                }
 ?>
-
+    <section class="member-content">
+<?php include "slider.php";?>
 <div class="member-content-right">
     <div class="member-content-right-list">
         <h1><?php echo "Workout Day: " . date("l, F jS, Y"); ?></h1>
         <form action="" method="POST">
             <input type="text" name="exercise" placeholder="Add exercise">
             <button type="submit" name="submit">Add</button>
+            <p class="mt-1">
+                <?php 
+                if(isset($status)){
+                echo $status;
+            }
+                ?>
+            </p>
         </form>
 
-        <!-- Display the list of added exercises -->
-        <div class="exercise-list">
-            <h2>Today's Exercises:</h2>
-            <ul>
-                <?php
-                if (isset($_SESSION['exercises'])) {
-                    foreach ($_SESSION['exercises'] as $exercise) {
-                        echo "<li>" . htmlspecialchars($exercise) . "</li>";
-                    }
-                }
-                ?>
-            </ul>
-        </div>
+              <h2 class="mt-2">Today's Exercises</h2>
+              <h3 class="mt-1">Total Duration : <?php echo $total_duration;?></h3>
+              <h3 class="mt-1">Total Calories Burned : <?php echo $total_caloriesBurned;?></h3>
+        
     </div>
 </div>
-        <div class="exercise-list">
-            <h2>Today's Exercises:</h2>
-            <ul>
-                <?php
-                if (isset($_SESSION['exercises'])) {
-                    foreach ($_SESSION['exercises'] as $exercise) {
-                        echo "<li>" . htmlspecialchars($exercise) . "</li>";
-                    }
-                }
-                ?>
-            </ul>
-        </div>
+        <
 
     </div>
 
