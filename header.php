d04564e843d40d2cfb025d8cc1e396079f6421df
DK <duykhanh02012004@gmail.com>
2024-01-11 02:10:24 +0000

    last change

--- header.php
+++ header.php
@@ -1,4 +1,7 @@
-  
+<?php 
+include "config.php";
+include "database.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,5 +12,24 @@
 </head>
 <body>
     <header>
+        <style>
+            .logout-button {
+    padding: 8px 15px;
+    background-color: #f44336; /* Màu nền đỏ */
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+    border-radius: 5px;
+    transition: background-color 0.3s;
+}
+
+.logout-button:hover {
+    background-color: #c0392b; /* Màu nền đỏ đậm hơn khi hover */
+}
+        </style>
         <h1>MEMBER</h1>
+        <a href="logout.php" class="logout-button">Logout</a>
     </header>
+
+</body>
+</html>
